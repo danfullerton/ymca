a80387a78cb2e9532850c6a1b27b5f905efd3e2a
Nathan Amick <github@nathanamick.com>
2012-01-30 21:32:46 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ymca::Application.config.secret_token = 'da4943c76d6cd130ced956f7066fb0690a3ab4dfed91c792b4f23fa607e2a32fa79fcf85e285a0e608fad7b2f66aabc42085a15f721bdf45561125e791b31061'
